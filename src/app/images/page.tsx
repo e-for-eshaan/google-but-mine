2aff2a5776c342cc94d54909b63935670c3f108c
e-for-eshaan <eshaanyadav24@gmail.com>
2024-12-03 16:25:10 +0530

    feat: update layout in Home component to include an image and improve scrolling behavior

--- src/app/images/page.tsx
+++ src/app/images/page.tsx
@@ -6,9 +6,21 @@ export default async function Home() {
         <div className="relative min-h-screen flex justify-between items-center flex-col gap-2">
             <Navbar />
             <div className="w-full flex h-full flex-grow">
-                <div className="flex-1">left</div>
-                <div className="flex-1">
-                <ImageGrid />
+                <div className="flex-1" style={{
+                    maxHeight: "calc(100vh - 68px)",
+                    overflowY: "auto",
+                }}>
+                    <img
+                        src="https://picsum.photos/200/300"
+                        alt="left"
+                        className="object-cover w-full h-full"
+                    />
+                </div>
+                <div className="flex-1" style={{
+                    maxHeight: "calc(100vh - 68px)",
+                    overflowY: "auto",
+                }}>
+                    <ImageGrid />
                 </div>
             </div>
         </div>
