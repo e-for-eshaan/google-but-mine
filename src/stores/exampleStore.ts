2861bf99e75f75e5de3a7845f7708051dd8567cf
e-for-eshaan <eshaanyadav24@gmail.com>
2024-12-03 14:17:33 +0530

    feat: refactor store to AppState and add image drop visibility management

--- src/stores/exampleStore.ts
+++ src/stores/exampleStore.ts
@@ -1,15 +1,22 @@
 import { create } from 'zustand';
 
-interface ExampleState {
+interface AppState {
   count: number;
   increment: () => void;
   decrement: () => void;
+  imageDropVisible: boolean;
+  showImageDrop: () => void;
+  hideImageDrop: () => void;
 }
 
-const useExampleStore = create<ExampleState>((set) => ({
+const appStore = create<AppState>((set) => ({
   count: 0,
   increment: () => set((state) => ({ count: state.count + 1 })),
   decrement: () => set((state) => ({ count: state.count - 1 })),
+
+  imageDropVisible: false,
+  showImageDrop: () => set((state) => ({ imageDropVisible: !state.showImageDrop })),
+  hideImageDrop: () => set({ imageDropVisible: false }),
 }));
 
-export default useExampleStore;
+export default appStore;
